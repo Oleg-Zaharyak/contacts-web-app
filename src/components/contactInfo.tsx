0b7bd6e7fb7ab1ad342c1d274af1a2cb4cd4b444
Oleh-Zakhariak <oleg@sidepocket.com>
2024-08-04 16:39:13 +0300

    Moved the contact info component to a separate file

---
+++ src/components/contactInfo.tsx
@@ -0,0 +1,111 @@
+import React, { FC } from "react";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import CustomButton from "./customButton";
+import CustomInput from "./customInput";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { PiUserCircleLight } from "react-icons/pi";
+import { useNavigate } from "react-router-dom";
+
+type ContactInfoProps = {
+  data: {
+    avatar_url: string;
+    fields: {
+      "first name": { value: string }[];
+      "last name": { value: string }[];
+      email: { value: string }[];
+    };
+    tags2: string[];
+    tags: {
+      id: string;
+      tag: string;
+    }[];
+  };
+};
+
+const ContactInfo: FC<ContactInfoProps> = ({ data }) => {
+  const navigate = useNavigate();
+
+  const validationSchema = Yup.object().shape({
+    tagText: Yup.string().min(2, "Too Short!").max(30, "Too Long!"),
+  });
+
+  return (
+    <div className="w-[430px] flex flex-col gap-y-6 relative">
+      <div className="flex gap-x-3">
+        {data.avatar_url && data.avatar_url ? (
+          <img
+            src={data.avatar_url}
+            className="rounded-full w-[70px]"
+            alt="user-avatar"
+          />
+        ) : (
+          <PiUserCircleLight className="text-[70px]" />
+        )}
+        <div className="flex flex-col justify-center font-medium">
+          <div>{`${data.fields["first name"][0].value} ${data.fields["last name"][0].value}`}</div>
+          <div>{data.fields.email[0].value}</div>
+        </div>
+      </div>
+
+      {data.tags && (
+        <div className="flex flex-col gap-y-3 mb-4">
+          <h1 className="font-medium">Tags</h1>
+          <ul className="col-start-2 flex flex-wrap gap-2">
+            {data.tags.map(
+              (tagInfo: { id: string; tag: string }, index: number) => (
+                <li
+                  key={`${tagInfo.id} `}
+                  className="px-3 text-[13px] font-medium rounded-[4px] bg-quick-silver"
+                >
+                  {tagInfo.tag}
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      )}
+      <Formik
+        initialValues={{ tagText: "" }}
+        validationSchema={validationSchema}
+        onSubmit={() => {}}
+      >
+        {({
+          values: { tagText },
+          touched,
+          isValid,
+          errors,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => {
+          const tagErrorMessage = (touched.tagText && errors.tagText) || "";
+          const disabledButton = !(isValid && !!tagText);
+          return (
+            <form className="flex flex-col gap-y-5" onSubmit={handleSubmit}>
+              <CustomInput
+                placeholder="Add new Tag"
+                type="text"
+                name="tagText"
+                value={tagText}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={tagErrorMessage}
+              />
+              <CustomButton
+                title="Add Tag"
+                type="submit"
+                disabled={disabledButton}
+              />
+            </form>
+          );
+        }}
+      </Formik>
+      <IoIosCloseCircleOutline
+        onClick={() => navigate("/")}
+        className="cursor-pointer absolute top-[10px] right-0 text-[22px]"
+      />
+    </div>
+  );
+};
+export default ContactInfo;
--- src/pages/contactsInfoPage.tsx
+++ src/pages/contactsInfoPage.tsx
@@ -1,103 +1,26 @@
 import { FC } from "react";
-import CustomInput from "../components/customInput";
-import CustomButton from "../components/customButton";
-import { Formik } from "formik";
-import { IoIosCloseCircleOutline } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
-import { PiUserCircleLight } from "react-icons/pi";
-import * as Yup from "yup";
+import ContactInfo from "../components/contactInfo";
 
 const ContactInfoPage: FC = () => {
   const data = {
-    avatarUrl: "",
-    firstName: "First Name",
-    lastName: "Last Name",
-    email: "email@gmail.com",
-    tags: ["some 1", "some 2"],
+    avatar_url: "#",
+    fields: {
+      "first name": [{ value: "dsad" }],
+      "last name": [{ value: "dasd" }],
+      email: [{ value: "dsad@dasd.com" }],
+    },
+    tags2: ["dsada"],
+    tags: [
+      {
+        id: "dsad",
+        tag: "dsad",
+      },
+    ],
   };
 
-  const { firstName, lastName, email, avatarUrl, tags } = data;
-
-  const validationSchema = Yup.object().shape({
-    tagText: Yup.string().min(2, "Too Short!").max(30, "Too Long!"),
-  });
-
-  const navigate = useNavigate();
   return (
     <div className="w-screen flex justify-center pt-9 px-8">
-      <div className="w-[430px] flex flex-col gap-y-6 relative">
-        <div className="flex gap-x-3">
-          {avatarUrl ? (
-            <img
-              src={avatarUrl}
-              className="rounded-full w-[70px]"
-              alt="user-avatar"
-            />
-          ) : (
-            <PiUserCircleLight className="text-[70px]" />
-          )}
-          <div className="flex flex-col justify-center font-medium">
-            <div>{`${firstName} ${lastName}`}</div>
-            <div>{email}</div>
-          </div>
-        </div>
-
-        {tags && (
-          <div className="flex flex-col gap-y-3 mb-4">
-            <h1 className="font-medium">Tags</h1>
-            <ul className="col-start-2 flex flex-nowrap gap-2">
-              {tags.map((tagText: string, index: number) => (
-                <li
-                  key={`${tagText} ${index}`}
-                  className="px-3 text-[13px] font-medium rounded-[4px] bg-quick-silver"
-                >
-                  {tagText}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-        <Formik
-          initialValues={{ tagText: "" }}
-          onSubmit={() => {}}
-          validationSchema={validationSchema}
-        >
-          {({
-            values: { tagText },
-            touched,
-            isValid,
-            errors,
-            handleChange,
-            handleBlur,
-          }) => {
-            const tagErrorMessage = (touched.tagText && errors.tagText) || "";
-            const disabledButton = !(isValid && !!tagText);
-            return (
-              <>
-                <CustomInput
-                  placeholder="Add new Tag"
-                  type="text"
-                  name="tagText"
-                  value={tagText}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  error={tagErrorMessage}
-                />
-                <CustomButton
-                  title="Add Tag"
-                  type="submit"
-                  disabled={disabledButton}
-                />
-              </>
-            );
-          }}
-        </Formik>
-
-        <IoIosCloseCircleOutline
-          onClick={() => navigate("/")}
-          className="cursor-pointer absolute top-[10px] right-0 text-[22px]"
-        />
-      </div>
+      <ContactInfo data={data} />
     </div>
   );
 };
